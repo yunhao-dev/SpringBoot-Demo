ea8d855992fb7705f8fe90e468a2f3e83f7d5f29
yunhao-dev <2384982531@qq.com>
2024-11-05 00:13:44 +0800

    init commit

---
+++ src/main/java/cn/yunhao/demo/Application.java
@@ -0,0 +1,16 @@
+package cn.yunhao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: start class
+ * @Author: yunhao_dev
+ * @Date: 2024/11/5
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
